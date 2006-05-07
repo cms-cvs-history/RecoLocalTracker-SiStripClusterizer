fe6a593dac98449c02a53ab482d86e22f771b520
Domenico Giordano <sha1-07f7bb78311ae556a5edf2d08855b34552c86a25@cern.ch>
2006-05-07 20:42:33 +0000

    test module TestCluster

---
+++ test/TestCluster.cc
@@ -0,0 +1,208 @@
+#include "RecoLocalTracker/SiStripClusterizer/test/TestCluster.h"
+
+namespace cms{
+  TestCluster::TestCluster(edm::ParameterSet const& conf): 
+    conf_(conf),
+    filename_(conf.getParameter<std::string>("fileName")), 
+    SiStripNoiseService_(conf),
+    SiStripPedestalsService_(conf)
+  {};
+
+  TestCluster::~TestCluster(){};
+  
+  void TestCluster::beginJob( const edm::EventSetup& es ) {
+    
+    SiStripNoiseService_.configure(es);
+    SiStripPedestalsService_.configure(es);
+    
+    myFile = new TFile(filename_.c_str(),"RECREATE");
+    myFile->cd();    
+    //get geom    
+    es.get<TrackerDigiGeometryRecord>().get( tkgeom );
+    edm::LogInfo("TestCluster") << "[TestCluster::beginJob] There are "<<tkgeom->dets().size() <<" detectors instantiated in the geometry" << std::endl;  
+    
+
+    edm::ParameterSet Parameters;
+    for(TrackerGeometry::DetContainer::const_iterator it = tkgeom->dets().begin(); it != tkgeom->dets().end(); it++){           
+      uint32_t detid=((*it)->geographicalId()).rawId();       
+      char name[128];
+      
+      const StripGeomDetUnit* _StripGeomDetUnit = dynamic_cast<const StripGeomDetUnit*>(tkgeom->idToDetUnit(DetId(detid)));
+      if (_StripGeomDetUnit==0){
+	edm::LogError("TestCluster")<< "[TestCluster::beginJob] the detID " << detid << " doesn't seem to belong to Tracker" << std::endl; 
+	continue;
+      }
+
+      sprintf(name,"ClusterSignal_%s_%d",_StripGeomDetUnit->type().name().c_str(),detid);    
+      edm::LogInfo("TestCluster") << "[TestCluster::beginJob] histo name" << name; 
+      Parameters =  conf_.getParameter<edm::ParameterSet>("TH1ClusterSignal");
+      _TH1F_ClusterSignal_m[detid] = new TH1F(name,name,
+					      Parameters.getParameter<int32_t>("Nbinx"),
+					      Parameters.getParameter<double>("xmin"),
+					      Parameters.getParameter<double>("xmax")
+					      );
+
+      sprintf(name,"ClusterStoN_%s_%d",_StripGeomDetUnit->type().name().c_str(),detid);
+      Parameters =  conf_.getParameter<edm::ParameterSet>("TH1ClusterStoN");
+      _TH1F_ClusterStoN_m[detid] = new TH1F(name,name,
+					    Parameters.getParameter<int32_t>("Nbinx"),
+					    Parameters.getParameter<double>("xmin"),
+					    Parameters.getParameter<double>("xmax")
+					    );
+
+      sprintf(name,"PedestalsProfile_%s_%d",_StripGeomDetUnit->type().name().c_str(),detid);
+      Parameters =  conf_.getParameter<edm::ParameterSet>("TH1PedestalsProfile");
+      _TH1F_PedestalsProfile_m[detid] = new TH1F(name,name,
+						 Parameters.getParameter<int32_t>("Nbinx"),
+						 Parameters.getParameter<double>("xmin"),
+						 Parameters.getParameter<double>("xmax")
+						 );
+
+      sprintf(name,"NoisesProfile_%s_%d",_StripGeomDetUnit->type().name().c_str(),detid);
+      Parameters =  conf_.getParameter<edm::ParameterSet>("TH1NoisesProfile");
+      _TH1F_NoisesProfile_m[detid] = new TH1F(name,name,
+					      Parameters.getParameter<int32_t>("Nbinx"),
+					      Parameters.getParameter<double>("xmin"),
+					      Parameters.getParameter<double>("xmax")
+					      );
+    }
+
+    Parameters =  conf_.getParameter<edm::ParameterSet>("TH1Noises");
+    _TH1F_Noises =  new TH1F("Noises","Noises",
+			     Parameters.getParameter<int32_t>("Nbinx"),
+			     Parameters.getParameter<double>("xmin"),
+			     Parameters.getParameter<double>("xmax")
+			     );
+  }
+  
+  void TestCluster::endJob() {  
+    edm::LogInfo("TestCluster") << "[TestCluster::endJob] >>> ending histograms" << std::endl;
+
+    myFile->cd();
+
+    for(TrackerGeometry::DetContainer::const_iterator it = tkgeom->dets().begin(); it != tkgeom->dets().end(); it++){           
+
+      //Get DetID
+      uint32_t detid=((*it)->geographicalId()).rawId();    
+      
+      int numStrips;
+      
+      //Get numStrips and verify that detid belongs to tracker
+      const StripGeomDetUnit*_StripGeomDetUnit = dynamic_cast<const StripGeomDetUnit*>(tkgeom->idToDetUnit(DetId(detid)));
+      if (_StripGeomDetUnit==0)
+	continue;
+      else{
+	numStrips = _StripGeomDetUnit->specificTopology().nstrips(); // det module number of strips
+      }
+
+      edm::LogInfo("TestCluster") << "[TestCluster::endJob] Nstrip for detID " << detid << " of name " <<  _StripGeomDetUnit->type().name().c_str() << " is " << numStrips;
+
+      for (int istrip=0;istrip<numStrips;istrip++){
+
+	try{
+	  //Fill Pedestals
+	  edm::LogInfo("TestCluster") << "[TestCluster::endJob] Fill Ped detid " << detid << " strip " << istrip;
+	  _TH1F_PedestalsProfile_m.find(detid)->second->Fill(istrip,SiStripPedestalsService_.getPedestal(detid,istrip));
+	  //Fill Noises
+	  edm::LogInfo("TestCluster") << "[TestCluster::endJob] Fill Noise detid " << detid << " strip " << istrip;
+	  _TH1F_NoisesProfile_m.find(detid)->second->Fill(istrip,SiStripNoiseService_.getNoise(detid,istrip));
+	  
+	  _TH1F_Noises->Fill(SiStripNoiseService_.getNoise(detid,istrip));
+	  //check
+	  if (SiStripNoiseService_.getNoise(detid,istrip) != SiStripPedestalsService_.getNoise(detid,istrip)) {
+	    edm::LogError("TestCluster") << "[TestCluster::endJob]  ERROR for detid " 
+					 << detid << " strip " << istrip 
+					 << "SiStripNoise and SiStripPedestal are different " 
+					 << SiStripNoiseService_.getNoise(detid,istrip) << " " 
+					 << SiStripPedestalsService_.getNoise(detid,istrip);
+	  }	
+	}
+	catch(cms::Exception& e){
+	  edm::LogError("TestCluster") << "[TestCluster::endJob]  cms::Exception:  DetName " << _StripGeomDetUnit->type().name() << " " << e.what() ;
+	}
+      }
+      edm::LogInfo("TestCluster") << "[TestCluster::endJob]  prima di remove";
+      //
+      //Remove det with low entries
+      {
+	std::map<uint32_t,TH1F*>::iterator hiter = _TH1F_ClusterSignal_m.find(detid);
+	if (hiter->second->GetMean()==0 || hiter->second->GetEntries() < 200){
+	  edm::LogError("TestCluster") << "[TestCluster::endJob]  " 
+				       << "il det " << hiter->first 
+				       << " non ha entries con media maggiore di zero || entries > 200";
+	  delete hiter->second;
+	  _TH1F_ClusterSignal_m.erase(hiter);
+	}
+      }
+      {
+	std::map<uint32_t,TH1F*>::iterator hiter = _TH1F_ClusterStoN_m.find(detid);
+	if (hiter->second->GetMean()==0 || hiter->second->GetEntries() < 200){
+	  edm::LogError("TestCluster") << "[TestCluster::endJob]  " 
+				       << "il det " << hiter->first 
+				       << " non ha entries con media maggiore di zero || entries > 200";
+	  delete hiter->second;
+	  _TH1F_ClusterStoN_m.erase(hiter);
+	}
+      }
+      {
+	std::map<uint32_t,TH1F*>::iterator hiter = _TH1F_PedestalsProfile_m.find(detid);
+	if (hiter->second->GetEntries() == 0){
+	  edm::LogError("TestCluster") << "[TestCluster::endJob]  " 
+				       << "il det " << hiter->first 
+				       << " non ha entries con media maggiore di zero || entries > 200";
+	  delete hiter->second;
+	  _TH1F_PedestalsProfile_m.erase(hiter);
+	}
+      }
+      {
+	std::map<uint32_t,TH1F*>::iterator hiter = _TH1F_NoisesProfile_m.find(detid);
+	if (hiter->second->GetEntries() == 0){
+	  edm::LogError("TestCluster") << "[TestCluster::endJob]  " 
+				       << "il det " << hiter->first 
+				       << " non ha entries con media maggiore di zero || entries > 200";
+	  delete hiter->second;
+	  _TH1F_NoisesProfile_m.erase(hiter);
+	}
+      }      
+    }
+
+    myFile->ls();
+    myFile->Write();
+    myFile->Close();
+  }
+
+  void TestCluster::analyze(const edm::Event& e, const edm::EventSetup& es) {
+    edm::LogInfo("TestCluster") << "[TestCluster::analyse]  " << "Run " << e.id().run() << " Event " << e.id().event() << std::endl;
+    
+    edm::Handle< edm::DetSetVector<SiStripCluster> >  input;
+    e.getByLabel("ThreeThresholdClusterizer",input);
+    
+    //Loop on Dets
+    edm::DetSetVector<SiStripCluster>::const_iterator DSViter=input->begin();
+    for (; DSViter!=input->end();DSViter++){
+      uint32_t detid = DSViter->id;
+      
+      float clusiz=0;
+      float Signal=0;
+      float StoN=0;
+      
+      //Loop on Clusters
+      for(edm::DetSet<SiStripCluster>::const_iterator ic = DSViter->data.begin(); ic!=DSViter->data.end(); ic++) {
+
+	clusiz = ic->amplitudes().size();
+	int barycenter=((int)ic->barycenter())%128;
+	if ( barycenter>3 && barycenter<125){
+	  const std::vector<short> amplitudes=ic->amplitudes();
+	  for(size_t i=0; i<amplitudes.size();i++){
+	    Signal+=amplitudes[i];
+	    StoN+=amplitudes[i]/SiStripNoiseService_.getNoise(detid,i);
+	  }
+	  //Fill Histos
+	  _TH1F_ClusterSignal_m.find(detid)->second->Fill(Signal);
+	  _TH1F_ClusterStoN_m.find(detid)->second->Fill(StoN);
+	}
+      }
+    }
+  }
+}
+
---
+++ test/TestCluster.h
@@ -0,0 +1,84 @@
+#ifndef TestCluster_h
+#define TestCluster_h
+
+#include "FWCore/Utilities/interface/Exception.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/Handle.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/MakerMacros.h" 
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "Geometry/TrackerGeometryBuilder/interface/TrackerGeometry.h"  
+#include "Geometry/Records/interface/TrackerDigiGeometryRecord.h"
+#include "Geometry/CommonDetUnit/interface/GeomDetUnit.h"
+#include "Geometry/TrackerGeometryBuilder/interface/StripGeomDetUnit.h"
+#include "Geometry/CommonTopologies/interface/StripTopology.h"
+#include "Geometry/CommonDetUnit/interface/GeomDetType.h"
+//needed for the geometry:
+#include "DataFormats/DetId/interface/DetId.h"
+#include "DataFormats/SiStripDetId/interface/StripSubdetector.h"
+#include "DataFormats/SiStripDetId/interface/TECDetId.h"
+#include "DataFormats/SiStripDetId/interface/TIBDetId.h"
+#include "DataFormats/SiStripDetId/interface/TIDDetId.h"
+#include "DataFormats/SiStripDetId/interface/TOBDetId.h"
+
+//Data Formats
+#include "DataFormats/Common/interface/DetSetVector.h"
+#include "DataFormats/SiStripDigi/interface/SiStripDigi.h"
+#include "DataFormats/SiStripCluster/interface/SiStripCluster.h"
+
+//SiStripPedestalsService
+#include "RecoLocalTracker/SiStripZeroSuppression/interface/SiStripPedestalsService.h"
+#include "RecoLocalTracker/SiStripClusterizer/interface/SiStripNoiseService.h"
+
+#include "TROOT.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TBranch.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include "TString.h"
+
+#include "vector"
+#include <memory>
+#include <string>
+#include <iostream>
+
+namespace cms{
+  class TestCluster : public edm::EDAnalyzer
+    {
+    public:
+      
+      TestCluster(const edm::ParameterSet& conf);
+  
+      ~TestCluster();
+      
+      void beginJob( const edm::EventSetup& es );
+      
+      void endJob();
+      
+      void analyze(const edm::Event& e, const edm::EventSetup& c);
+  
+    private:
+  
+      edm::ParameterSet conf_;
+      const StripTopology* topol;
+      edm::ESHandle<TrackerGeometry> tkgeom;
+      std::map< uint32_t, TH1F* >  _TH1F_ClusterSignal_m;
+      std::map< uint32_t, TH1F* >  _TH1F_ClusterStoN_m;
+      std::map< uint32_t, TH1F* >  _TH1F_PedestalsProfile_m;
+      std::map< uint32_t, TH1F* >  _TH1F_NoisesProfile_m;
+      std::vector<TH1F*> _TH1F_Noises_v;
+      std::vector<TH1F*> _TH1F_ClusterSignal+v;
+      std::vector<TH1F*> _TH1F_ClusterStoN_v;
+      std::string filename_;
+      TFile* myFile;
+
+      SiStripNoiseService SiStripNoiseService_;  
+      SiStripPedestalsService SiStripPedestalsService_;  
+    };
+}
+#endif
