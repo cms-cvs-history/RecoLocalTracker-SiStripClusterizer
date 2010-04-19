0960eba79b2666071f928c3fc42bb78a0c607f44
cmsbuild <>
2010-04-19 21:48:50 +0000

    This commit was manufactured by cvs2git to create tag 'RecoLocalTracker- SiStripClusterizer-V14-03-05'.
    Sprout from master 2010-02-11 00:14:07 UTC William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch> 'Adapt to moved framework header'
    Cherrypick from master 2010-04-19 21:48:46 UTC William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch> 'Temporary backout to get minimal tag':
        BuildFile.xml
        plugins/BuildFile.xml
        plugins/SealModules.cc
        python/SiStripClusterizer_RealData_cfi.py
        python/SiStripClusterizer_SimData2_cfi.py
        python/SiStripClusterizer_SimData_cfi.py
        test/BuildFile.xml
        test/SealModules.cc
    Delete:
        BuildFile
        plugins/BuildFile
        src/SiStripApvShotCleaner.cc
        test/BuildFile

--- plugins/SealModules.cc
+++ plugins/SealModules.cc
@@ -1,10 +1,7 @@
-#include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-DEFINE_SEAL_MODULE();
-
 #include "RecoLocalTracker/SiStripClusterizer/plugins/SiStripClusterizer.h"
-DEFINE_ANOTHER_FWK_MODULE(SiStripClusterizer);
+DEFINE_FWK_MODULE(SiStripClusterizer);
 
 #include "RecoLocalTracker/SiStripClusterizer/plugins/SiStripClusterProducer.h"
-DEFINE_ANOTHER_FWK_MODULE(SiStripClusterProducer);
+DEFINE_FWK_MODULE(SiStripClusterProducer);
