63fb47d0c280d28bb9dd8b73797169aeff23b5a5
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2012-04-25 13:39:40 +0000

    Initial changes for pileup memory reduction

--- python/SiStripClusterizer_SimData2_cfi.py
+++ python/SiStripClusterizer_SimData2_cfi.py
@@ -5,7 +5,7 @@ from RecoLocalTracker.SiStripClusterizer.DefaultClusterizer_cff import *
 siStripClusters = cms.EDProducer("SiStripClusterizer",
                                Clusterizer = DefaultClusterizer,
                                DigiProducersList = cms.VInputTag(
-    cms.InputTag('simSiStripDigis','ZeroSuppressed'),
+    cms.InputTag('mix', 'simSiStripDigisZeroSuppressed'),
     cms.InputTag('siStripZeroSuppression','VirginRaw'),
     cms.InputTag('siStripZeroSuppression','ProcessedRaw'),
     cms.InputTag('siStripZeroSuppression','ScopeMode'))
